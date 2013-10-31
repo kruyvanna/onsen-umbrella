e9575708f66780094a11f40d8396cf94e3b51725
Kruy Vanna <kruyvanna@gmail.com>
2013-10-31 14:03:02 +0900

    add batch file for npm link and git pull

---
+++ link.sh
@@ -0,0 +1,59 @@
+
+cd button
+npm link --force
+cd ../button-bar
+npm link --force
+cd ../button-bar-base
+npm link --force
+cd ../checkbox
+npm link --force
+cd ../icon-button
+npm link --force
+cd ../list
+npm link --force
+cd ../list-base
+npm link --force
+cd ../navigation-bar
+npm link --force
+cd ../notification
+npm link --force
+cd ../notification-base
+npm link --force
+cd ../radio-button
+npm link --force
+cd ../range
+npm link --force
+cd ../search-input
+npm link --force
+cd ../switch-base
+npm link --force
+cd ../switch
+npm link --force
+cd ../tab-bar
+npm link --force
+cd ../text-input
+npm link --force
+cd ../textarea
+npm link --force
+cd ../theme
+npm link --force
+
+cd ../topcoat
+npm link --force topcaot-button
+npm link --force topcaot-button-bar
+npm link --force topcaot-button-bar-base
+npm link --force topcaot-checkbox
+npm link --force topcaot-icon-button
+npm link --force topcaot-list
+npm link --force topcaot-list-base
+npm link --force topcaot-navigation-bar
+npm link --force topcaot-notification
+npm link --force topcaot-notification-base
+npm link --force topcaot-radio-button
+npm link --force topcaot-range
+npm link --force topcaot-search-input
+npm link --force topcaot-switch-base
+npm link --force topcaot-tab-bar
+npm link --force topcaot-text-input
+npm link --force topcaot-textarea
+npm link --force topcaot-theme
